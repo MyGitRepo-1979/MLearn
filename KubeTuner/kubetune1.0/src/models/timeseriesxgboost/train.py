654879d22d19e54fff26a18f65a38f3a67ede16e
Navya Mamgain <flair99@gmail.com>
2025-06-12 08:55:15 +0530

    timeseriesXGboost regression

---
+++ KubeTuner/kubetune1.0/src/models/timeseriesxgboost/train.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+import xgboost as xgb
+from xgboost import plot_importance, plot_tree
+from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pathlib import Path
+import evaluate as evaluate
+import joblib
+
+def create_features(df, label=None):
+    """
+    Creates time series features from datetime index
+    """
+    df['date'] = df.index
+    df['hour'] = df['date'].dt.hour
+    df['dayofweek'] = df['date'].dt.dayofweek
+    df['quarter'] = df['date'].dt.quarter
+    df['month'] = df['date'].dt.month
+    df['year'] = df['date'].dt.year
+    df['dayofyear'] = df['date'].dt.dayofyear
+    df['dayofmonth'] = df['date'].dt.day
+    df['weekofyear'] = df['date'].dt.isocalendar().week
+
+    X = df[['hour','dayofweek','quarter','month','year',
+           'dayofyear','dayofmonth','weekofyear']]
+    if label:
+        y = df[label]
+        return X, y
+    return X
+
+def train_memory_model():
+    # -----------------------------
+    # 1. Load and Preprocess Data
+    # -----------------------------
+    # Path to your Excel file
+    base_dir_input = Path(__file__).resolve().parents[3]
+    file_path = base_dir_input / 'data' / 'AEP_Energy_consumption.xlsx'
+
+    df = pd.read_excel(file_path, sheet_name="AEP_hourly",index_col=[0], parse_dates=[0])
+
+    split_date = '01-Jan-2015'
+    df_train = df.loc[df.index <= split_date].copy()
+    df_test = df.loc[df.index > split_date].copy()
+
+    X_train, y_train = create_features(df_train, label='AEP_MW')
+    X_test, y_test = create_features(df_test, label='AEP_MW')
+
+    model = xgb.XGBRegressor(n_estimators=1000)
+    model.fit(X_train, y_train,
+        eval_set=[(X_train, y_train), (X_test, y_test)],
+        verbose=True) # Change verbose to True if you want to see it train
+
+    # Save the  model
+    model_path = Path(__file__).resolve().parents[0] / 'output' / 'kubetune_xgb_model_memoryusage.pkl'
+    model_path.parent.mkdir(parents=True, exist_ok=True)  # Ensure the directory exists
+    joblib.dump(model, model_path)
+
+    # evaluate model
+    evaluate.evaluate_model(model_path,X_test, y_test)
+
+if __name__ == "__main__":
+
+    print("------------------------------")
+    print("Training & Evaluating Decision Tree Models for Memory Usage...")
+    train_memory_model()
+
+    # print("------------------------------")
+    # print("Training & Evaluating Decision Tree Models for CPU Usage...")
+    # train_cpu_model()
+    # print("------------------------------")
+
+
+
